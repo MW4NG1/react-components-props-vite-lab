9d293dedf1dec95d3e828204901902a1f7f97e5c
MW4NG1 <msage3803@gmail.com>
2026-07-27 18:00:45 +0300

    fix Header component markup to use correct HTML tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from "react";
 // Header component
 function Header(props) {
     return (
-    <Header>
+    <header>
         {/* Display the blog name */}
         <h1>{props.name}</h1>
-    </Header>
+    </header>
     );
 }
 export default Header;
