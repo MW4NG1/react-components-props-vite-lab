fd6bceb7ec90c61b135589ec55e42e9eafb7ac39
MW4NG1 <msage3803@gmail.com>
2026-07-27 17:26:11 +0300

    add About, Article, ArticleList, and Header components

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+// About component
+function About() {
+    return (
+    <div>About</div>);
+}
+export default About;
---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+// ArticleList component
+function ArticleList() {
+    return (
+    <div>ArticleList</div>);
+}
+export default ArticleList;
